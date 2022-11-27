e5fc069043ea1cef3549bfb8c1eb092ed42ad522
Bienvenu Murenzi <bienvenu@paytrippa.com>
2022-11-28 01:20:52 +0200

    add custom icon

---
+++ Lab3/app/src/main/java/com/example/lab3/model/SampleData.kt
@@ -0,0 +1,10 @@
+package com.example.lab3.model
+
+val sampleBookData = listOf(
+    ToDo("1", "Go shopping", "Today 12:50", true),
+    ToDo("2", "Go shopping", "Today 12:50", false),
+    ToDo("3", "Go shopping", "Today 12:50", false),
+    ToDo("4", "Go shopping", "Today 12:50", false),
+    ToDo("5", "Go shopping", "Today 12:50", false),
+    ToDo("6", "Go shopping", "Today 12:50", false)
+)
---
+++ Lab3/app/src/main/java/com/example/lab3/model/ToDoViewModel.kt
@@ -0,0 +1,20 @@
+package com.example.lab3.model
+
+import androidx.compose.runtime.mutableStateListOf
+import androidx.lifecycle.ViewModel
+
+class ToDoViewModel: ViewModel() {
+    var todoList = mutableStateListOf<ToDo>()
+    fun add(newToDo: ToDo){
+        todoList.add(newToDo)
+    }
+
+    fun delete(toDo: ToDo){
+        todoList.remove(toDo)
+    }
+
+    fun markCompleted(toDo: ToDo)
+    {
+        toDo.completed = !toDo.completed
+    }
+}
